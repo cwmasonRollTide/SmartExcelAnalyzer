d0cde760693e1f0c07b8d763a15e1f911188294e
Connor <cdubyamason@gmail.com>
2024-10-28 12:23:35 -0400

    updates

--- Backend/Persistence/Hubs/ProgressHubWrapper.cs
+++ Backend/Persistence/Hubs/ProgressHubWrapper.cs
@@ -4,22 +4,35 @@ using Microsoft.Extensions.Logging;
 namespace Persistence.Hubs;
 
 public class ProgressHubWrapper(
-    IHubContext<ProgressHub> hubContext, 
+    IHubContext<ProgressHub> _hubContext, 
     ILogger<IProgressHubWrapper> _logger
 ) : IProgressHubWrapper
 {
-    private readonly IHubContext<ProgressHub> _hubContext = hubContext;
+    private const string ReceiveErrorMethod = "ReceiveError";
+    private const string ReceiveProgressMethod = "ReceiveProgress";
+    private const string ReceiveCompletionMethod = "ReceiveCompletion";
+    private const string ProgressCompleteMessage = "Progress complete";
+    private const string ProgressCompleteLogMessage = "Progress complete: {Message}";
+    private const string ProgressUpdateMessage = "Progress update: {Progress}/{Total}";
 
     public async Task SendProgress(double progress, double total) 
     {
-        _logger.LogInformation("Progress update: {Progress}/{Total}", progress, total);
-        if (progress == total) _logger.LogInformation("Progress complete: {Message}", "Progress complete");
-        await SendCompletion("Progress complete");
+        _logger.LogInformation(ProgressUpdateMessage, progress, total);
+
+        if (progress.Equals(total)) 
+            _logger.LogInformation(ProgressCompleteLogMessage, ProgressCompleteMessage);
+
+        await _hubContext
+            .Clients
+            .All
+            .SendAsync(
+                ReceiveProgressMethod, 
+                progress, 
+                total
+            );
     }
 
-    public async Task SendCompletion(string message) =>
-        await _hubContext.Clients.All.SendAsync("ReceiveCompletion", message);
+    public async Task SendError(string message) => await _hubContext.Clients.All.SendAsync(ReceiveErrorMethod, message);
 
-    public async Task SendError(string message) =>
-        await _hubContext.Clients.All.SendAsync("ReceiveError", message);
+    public async Task SendCompletion(string message) => await _hubContext.Clients.All.SendAsync(ReceiveCompletionMethod, message);
 }
