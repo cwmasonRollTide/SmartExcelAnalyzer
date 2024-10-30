90d084d6bc20480181cb2fc68541e3bff71d16d2
Connor <cdubyamason@gmail.com>
2024-10-29 23:08:05 -0400

    config changes

--- Frontend/jest.config.cjs
+++ Frontend/jest.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    transform: {
-      '^.+\\.(ts|tsx)$': 'babel-jest',
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    globals: {
-      'import.meta.env': global.importMetaEnv,
-    },
-    verbose: true
-  };
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  globals: {
+    'import.meta.env': global.importMetaEnv,
+  },
+  verbose: true, 
+};
