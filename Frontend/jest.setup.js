150f1752e6cc19d9a5870a7e5dfebfa2707d2eeb
Connor <cdubyamason@gmail.com>
2024-11-01 23:05:17 -0400

    updating conf

--- Frontend/jest.setup.js
+++ Frontend/jest.setup.js
@@ -1,4 +1,4 @@
 global.importMetaEnv = {
-  VITE_BASE_API_URL: 'http://localhost:5001',
-  VITE_SIGNALR_HUB_URL: 'http://localhost:5001/progressHub',
+  VITE_BASE_API_URL: 'http://backend:5001',
+  VITE_SIGNALR_HUB_URL: 'http://backend:5001/progressHub',
 };
