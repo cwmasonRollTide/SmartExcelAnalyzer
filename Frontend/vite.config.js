74956c4ef29546ca3ed0f12b2ded0988da6157e2
Connor <cdubyamason@gmail.com>
2024-11-10 04:31:33 -0500

    adding tracing and metrics

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     server: {
       port: 3000,
-      host: "0.0.0.0",
+      host: "traefik",
       strictPort: true,
       proxy: {
         "/api": {
