60529b5a45274b9b103f4b589c7fbcf985a4d3bf
Connor <cdubyamason@gmail.com>
2024-11-10 06:36:35 -0500

    please be actually close lol this is getting out of control

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,20 +8,20 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     server: {
       port: 3000,
-      host: "0.0.0.0",
+      host: "traefik",
       strictPort: true,
-      proxy: {
-        "/api": {
-          target: env.VITE_BASE_API_URL,
-          changeOrigin: true,
-          secure: false,
-        },
-        "/progressHub": {
-          secure: false,
-          changeOrigin: true,
-          target: env.VITE_SIGNALR_HUB_URL,
-        },
-      },
+      // proxy: {
+      //   "/api": {
+      //     target: env.VITE_BASE_API_URL,
+      //     changeOrigin: true,
+      //     secure: false,
+      //   },
+      //   "/progressHub": {
+      //     secure: false,
+      //     changeOrigin: true,
+      //     target: env.VITE_SIGNALR_HUB_URL,
+      //   },
+      // },
     },
     build: {
       outDir: "dist",
